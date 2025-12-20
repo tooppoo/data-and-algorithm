0e47a3c825549e8d86d39291d5b68dfe9aa50752
philomagi <tooppoo56@gmail.com>
2025-12-20 14:00:51 +0900

    impl desc

--- ts/sort/insert-sort.spec.ts
+++ ts/sort/insert-sort.spec.ts
@@ -1,11 +1,18 @@
 import { describe, expect } from 'vitest';
 import { it, fc } from '@fast-check/vitest';
-import { insertSort } from './insert-sort';
+import { insertSort, insertSortDesc } from './insert-sort';
 
 describe('insrt sort', () => {
   it.prop([fc.array(fc.integer())])('should sort array correctly', (arr) => {
     const sorted = [...arr].sort((a, b) => a - b);
 
     expect(insertSort(arr)).toEqual(sorted);
+  });
+});
+describe('insrt sort desc', () => {
+  it.prop([fc.array(fc.integer())])('should sort array correctly', (arr) => {
+    const sorted = [...arr].sort((a, b) => b - a);
+
+    expect(insertSortDesc(arr)).toEqual(sorted);
   });
 });
--- ts/sort/insert-sort.ts
+++ ts/sort/insert-sort.ts
@@ -12,3 +12,17 @@ export function insertSort(arr: number[]): number[] {
   }
   return arr;
 }
+
+export function insertSortDesc(arr: number[]): number[] {
+  for(let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while(j >= 0 && arr[j] < key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
